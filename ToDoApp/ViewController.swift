65d2a3230aa986a1f1827a503d69591e04c83c08
Evgenii Kolgin <mista.colgate@gmail.com>
2021-02-25 23:40:30 +0800

    add swipe actions all works

--- ToDoApp/ViewController.swift
+++ ToDoApp/ViewController.swift
@@ -4,9 +4,6 @@
 //
 //  Created by Evgenii Kolgin on 20.11.2020.
 //
-
-
-
 import UIKit
 import RealmSwift
 
@@ -16,73 +13,90 @@ class ViewController: UIViewController {
     
     var notes = [ToDoListItem]()
     
-    private let collectionView: UICollectionView = {
-        
-        var configuration = UICollectionLayoutListConfiguration(appearance: .grouped)
-        
-        configuration.trailingSwipeActionsConfigurationProvider = { indexPath in
-            let del = UIContextualAction(style: .destructive, title: "Delete") { action, view, completion in
-
-
-
-
-
-                completion(true)
-            }
-            return UISwipeActionsConfiguration(actions: [del])
-        }
-        
-        
-        let layout = UICollectionViewCompositionalLayout.list(using: configuration)
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.backgroundColor = .systemGroupedBackground
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        return collectionView
-    }()
+    private var collectionView: UICollectionView!
     
     var dataSource: UICollectionViewDiffableDataSource<Section, ToDoListItem>!
+    
+    var snapshot: NSDiffableDataSourceSnapshot<Section, ToDoListItem>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAddButton))
+        view.backgroundColor = .systemBackground
         title = "ToDoList"
         navigationController?.navigationBar.prefersLargeTitles = true
-        collectionView.delegate = self
+        
         
         setupCollectionView()
-        setupSubviews()
+        collectionView.delegate = self
         
         loadObjects()
         updateDataSource()
     }
     
     func setupCollectionView() {
-        let registration = UICollectionView.CellRegistration<UICollectionViewListCell, ToDoListItem> { (cell, indexPath, note) in
+        var layoutConfig = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        
+        layoutConfig.trailingSwipeActionsConfigurationProvider = { [unowned self] (indexPath) in
+            
+            guard let item = dataSource.itemIdentifier(for: indexPath) else {
+                return nil
+            }
+            
+            let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (action, view, completion) in
+                deleteSwipe(for: action, item: item)
+                completion(true)
+            }
+            
+            //
+            return UISwipeActionsConfiguration(actions: [deleteAction])
+        }
+        
+        layoutConfig.leadingSwipeActionsConfigurationProvider = { [unowned self] (indexPath) in
+            guard let item = dataSource.itemIdentifier(for: indexPath) else {
+                return nil
+            }
+            
+            let archiveAction = UIContextualAction(style: .normal, title: "Archived") { (action, view, completion) in
+                archiveSwipe(for: action, item: item)
+                completion(true)
+            }
+            archiveAction.backgroundColor = .systemGreen
+            return UISwipeActionsConfiguration(actions: [archiveAction])
+        }
+        
+        let listLayout = UICollectionViewCompositionalLayout.list(using: layoutConfig)
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: listLayout)
+        
+        view.addSubview(collectionView)
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 0.0),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0.0),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0.0),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0.0),
+        ])
+        
+        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, ToDoListItem> { (cell, indexPath, item) in
+            
             var content = cell.defaultContentConfiguration()
-            content.text = note.itemText
+            content.text = item.itemText
             cell.contentConfiguration = content
         }
         
-        dataSource = UICollectionViewDiffableDataSource<Section, ToDoListItem>(collectionView: collectionView) { (collectionView, indexPath, note) -> UICollectionViewCell? in
-            collectionView.dequeueConfiguredReusableCell(using: registration, for: indexPath, item: note)
-        }
-    }
-    
-    
-    func setupSubviews() {
-        view.addSubview(collectionView)
-        NSLayoutConstraint.activate([collectionView.topAnchor.constraint(equalTo: view.topAnchor),
-                                     collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-                                     collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-                                     collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor)])
+        dataSource = UICollectionViewDiffableDataSource<Section, ToDoListItem>(collectionView: collectionView, cellProvider: { (collectionView: UICollectionView, indexPath: IndexPath, identifier: ToDoListItem) -> UICollectionViewCell? in
+            let cell = collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: identifier)
+            // you can coondigure cell appearance here
+            return cell
+        })
     }
     
     func updateDataSource() {
-        
-        var snapshot = NSDiffableDataSourceSnapshot<Section, ToDoListItem>()
+        snapshot = NSDiffableDataSourceSnapshot<Section, ToDoListItem>()
         snapshot.appendSections([.main])
-        snapshot.appendItems(notes)
+        snapshot.appendItems(notes, toSection: .main)
         
         dataSource.apply(snapshot,animatingDifferences: true)
     }
@@ -101,12 +115,11 @@ class ViewController: UIViewController {
         }
     }
     
-    
     @objc private func didTapAddButton() {
         let alert = UIAlertController(title: "Add a new task", message: nil, preferredStyle: .alert)
         
         alert.addTextField { (textField:UITextField) in
-            textField.placeholder = "Enter your note"
+            textField.placeholder = "Enter your task"
         }
         let saveAction = UIAlertAction(title: "Save", style: .default, handler: { (action) in
             let noteTextField = alert.textFields![0] as UITextField
@@ -127,27 +140,14 @@ class ViewController: UIViewController {
 
 //MARK: - TableView Delegate
 extension ViewController: UICollectionViewDelegate {
-    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        collectionView.deselectItem(at: indexPath, animated: true)
         
+        guard let selectedItem = dataSource.itemIdentifier(for: indexPath) else {
+            collectionView.deselectItem(at: indexPath, animated: true)
+            return
+        }
         
-        let actionSheet = UIAlertController(title: "Actions", message: "What do you want to do?", preferredStyle: .actionSheet)
-        actionSheet.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { action in
-            
-            let item = self.notes[indexPath.row]
-            do {
-                try self.realm.write {
-                    self.realm.delete(item)
-                }
-            } catch  {
-                
-            }
-            self.loadObjects()
-            self.updateDataSource()
-            
-            
-        }))
+        let actionSheet = UIAlertController(title: selectedItem.itemText, message: "What do you want to do?", preferredStyle: .actionSheet)
         
         actionSheet.addAction(UIAlertAction(title: "Edit", style: .default, handler: { action in
             
@@ -175,15 +175,53 @@ extension ViewController: UICollectionViewDelegate {
                     }
                 }
             })
-            
-            let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: { (_) in
+                collectionView.deselectItem(at: indexPath, animated: true)
+            })
             
             alert.addAction(cancelAction)
             alert.addAction(saveAction)
             
             self.present(alert, animated: true, completion: nil)
         }))
+        
+        
+        actionSheet.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { action in
+            
+            let item = self.notes[indexPath.row]
+            do {
+                try self.realm.write {
+                    self.realm.delete(item)
+                }
+            } catch  {
+                
+            }
+            self.loadObjects()
+            self.updateDataSource()
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (_) in
+            collectionView.deselectItem(at: indexPath, animated: true)
+        }))
         present(actionSheet, animated: true, completion: nil)
+    }
+}
+
+extension ViewController {
+    func deleteSwipe(for action: UIContextualAction, item: ToDoListItem) {
+        do {
+            try self.realm.write {
+                self.realm.delete(item)
+            }
+        } catch  {
+            
+        }
+        self.loadObjects()
+        self.updateDataSource()
+    }
+    
+    func archiveSwipe(for action: UIContextualAction, item: ToDoListItem) {
+        
     }
 }
 
